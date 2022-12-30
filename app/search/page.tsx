0a26c269dbf2a7617db16c44eff6994eae27b54a
Gayathri R N <gayathri.9mar94@gmail.com>
2022-12-30 16:26:16 +0530

    fixes

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,7 +5,7 @@ import fetchNews from "../../lib/fetchNews";
 import NewsList from "../NewsList";
 
 async function SearchPage({ searchParams }: Props) {
-    const news: NewsResponse = await fetchNews("general", searchParams?.term, true);
+    const news: NewsResponse = await fetchNews("general", searchParams?.term, null, true);
     return (
         <div>
             <h1 className="headerTitle">Search Results for: {searchParams?.term}</h1>
