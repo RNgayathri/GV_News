38c9a029ccd403524d49451899d060d9d505f57b
Gayathri R N <gayathri.9mar94@gmail.com>
2023-02-28 18:58:58 +0530

    fixes

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -7,26 +7,26 @@ type Props = {
 
 function ArticlePage({ searchParams }: Props) {
     console.log(searchParams)
-    if (!searchParams) {
+    if (searchParams && Object.entries(searchParams).length == 0 || !searchParams) {
         return notFound();
     }
     const article: Article = searchParams;
     return (
         <article>
             <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-                {searchParams.image && (
-                    <img className="h-50 max-w-md mx-auto md:max-w-g lg:max-w-xl object-cover rounded-lg shadow-md" src={searchParams.image} alt={searchParams.title} />
+                {article.image && (
+                    <img className="h-50 max-w-md mx-auto md:max-w-g lg:max-w-xl object-cover rounded-lg shadow-md" src={article.image} alt={searchParams.title} />
                 )}
                 <div className="px-10">
-                    <h1 className="headerTitle px-0 no-underline pb-2">{searchParams.title}</h1>
+                    <h1 className="headerTitle px-0 no-underline pb-2">{article.title}</h1>
                     <div>
                         <h2 className="font-bold">
-                            By: {searchParams.author}
+                            By: {article.author}
                         </h2>
-                        <h2 className="font-bold pl-4">Source: {searchParams.source}</h2>
-                        <div className="pl-4"><LiveTimeStamp time={searchParams.published_at} /></div>
+                        <h2 className="font-bold pl-4">Source: {article.source}</h2>
+                        {/* <div className="pl-4"><LiveTimeStamp time={article.published_at} /></div> */}
                     </div>
-                    <p className="pt-4">{searchParams.description}</p>
+                    <p className="pt-4">{article.description}</p>
                 </div>
             </section>
         </article>
