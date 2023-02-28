d80235b679510b531ead9a185c6e36b25e5fbf33
Gayathri R N <gayathri.9mar94@gmail.com>
2023-02-28 18:41:05 +0530

    testing

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -14,19 +14,19 @@ function ArticlePage({ searchParams }: Props) {
     return (
         <article>
             <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-                {article.image && (
-                    <img className="h-50 max-w-md mx-auto md:max-w-g lg:max-w-xl object-cover rounded-lg shadow-md" src={article.image} alt={article.title} />
+                {searchParams.image && (
+                    <img className="h-50 max-w-md mx-auto md:max-w-g lg:max-w-xl object-cover rounded-lg shadow-md" src={searchParams.image} alt={searchParams.title} />
                 )}
                 <div className="px-10">
-                    <h1 className="headerTitle px-0 no-underline pb-2">{article.title}</h1>
+                    <h1 className="headerTitle px-0 no-underline pb-2">{searchParams.title}</h1>
                     <div>
                         <h2 className="font-bold">
-                            By: {article.author}
+                            By: {searchParams.author}
                         </h2>
-                        <h2 className="font-bold pl-4">Source: {article.source}</h2>
-                        <p className="pl-4"><LiveTimeStamp time={article.published_at} /></p>
+                        <h2 className="font-bold pl-4">Source: {searchParams.source}</h2>
+                        <p className="pl-4"><LiveTimeStamp time={searchParams.published_at} /></p>
                     </div>
-                    <p className="pt-4">{article.description}</p>
+                    <p className="pt-4">{searchParams.description}</p>
                 </div>
             </section>
         </article>
